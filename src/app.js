fc6abdad995d91115c37d6b583bc42ae0d1e1b9f
rakesh-modugu <rakeshmodugu94@gmail.com>
2025-11-24 16:08:44 +0530

    Add response handling in /user endpoint middleware

--- src/app.js
+++ src/app.js
@@ -32,28 +32,39 @@ const app = express()
 // })/
 
 
-app.get('/',(req,res) => {
-    res.send("Hello from Rakesh server at root endpoint")
-})
+// app.get('/',(req,res) => {
+//     res.send("Hello from Rakesh server at root endpoint")
+// })
 
-app.get('/data',(req,res) => {
-    res.json({name: "Rakesh", age: 24, city: "Bangalore"})
-})
+// app.get('/data',(req,res) => {
+//     res.json({name: "Rakesh", age: 24, city: "Bangalore"})
+// })
 
-app.post('/data',(req,res) => {
-    res.json({message: "This is a POST request at /data endpoint"})
-})
+// app.post('/data',(req,res) => {
+//     res.json({message: "This is a POST request at /data endpoint"})
+// })
 
-app.put('/data',(req,res) => {
-    res.json({message: "This is a PUT request at /data endpoint"})
-})
+// app.put('/data',(req,res) => {
+//     res.json({message: "This is a PUT request at /data endpoint"})
+// })
 
-app.delete('/data',(req,res) => {
-    res.json({message: "This is a DELETE request at /data endpoint"})
-})
-app.patch('/data',(req,res) => {
-    res.json({message: "This is a PATCH request at /data endpoint"})
-})
+// app.delete('/data',(req,res) => {
+//     res.json({message: "This is a DELETE request at /data endpoint"})
+// })
+// app.patch('/data',(req,res) => {
+//     res.json({message: "This is a PATCH request at /data endpoint"})
+// })
+
+app.use('/user',(req,res, next) => {
+    //res.send('1 Response!')
+    // console.log('This is user endpoint')
+    next()
+},(
+req,res) => {
+    res.send('2nd response!')
+}
+ )
+
 
 app.listen(3000,() => {
     console.log(`Server is running on port 3000: http://localhost:3000`);
