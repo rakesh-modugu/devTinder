c6765b523be31fa67ca6783ee2eaa194550345f2
rakesh-modugu <rakeshmodugu94@gmail.com>
2025-11-24 11:56:24 +0530

    get ,put ,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -2,14 +2,58 @@ const express = require('express')
 
 const app = express()
 
-app.use((req,res) => {
-    res.send("Hello from rakesh server ")
+// app.use((req,res) => {
+//     res.send("Hello from rakesh server ")
+// })
+
+
+// app.use('/data',(req,res) => {
+//     res.json({name: "Rakesh", age: 24, city: "Bangalore"})
+// })  
+
+// app.use('/hello/3', (req,res) => {
+//     res.send('hello are three')
+// })
+
+
+
+// app.use('/hello', (req,res) => {
+//     res.send("Hello from /hello endpoint")
+// })
+
+
+// app.use('/xyz', (req,res)=> {
+//     res.send('some xyz are there')
+// })
+// app.use('/',(req,res) => {
+//     res.send("Hello from Rakesh server at root endpoint")
+
+
+// })/
+
+
+app.get('/',(req,res) => {
+    res.send("Hello from Rakesh server at root endpoint")
 })
 
-
-app.use('/data',(req,res) => {
+app.get('/data',(req,res) => {
     res.json({name: "Rakesh", age: 24, city: "Bangalore"})
-})  
+})
+
+app.post('/data',(req,res) => {
+    res.json({message: "This is a POST request at /data endpoint"})
+})
+
+app.put('/data',(req,res) => {
+    res.json({message: "This is a PUT request at /data endpoint"})
+})
+
+app.delete('/data',(req,res) => {
+    res.json({message: "This is a DELETE request at /data endpoint"})
+})
+app.patch('/data',(req,res) => {
+    res.json({message: "This is a PATCH request at /data endpoint"})
+})
 
 app.listen(3000,() => {
     console.log(`Server is running on port 3000: http://localhost:3000`);
